7e80ee1fbcebeea1fac778bc97b00497159dceef
KushguptaPST <kushg0082@gmail.com>
2026-04-27 12:47:07 +0530

    add Booking model and user Model

---
+++ backend/controllers/vendorControllers/vendorController.js
@@ -0,0 +1,134 @@
+import User from "../../models/userModel.js";
+import bcryptjs from "bcryptjs";
+import Jwt from "jsonwebtoken";
+import { errorHandler } from "../../utils/error.js";
+
+
+const expireDate = new Date(Date.now() + 3600000);
+
+export const vendorSignup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  try {
+    const hadshedPassword = bcryptjs.hashSync(password, 10);
+    const user = await User.create({
+      username,
+      password: hadshedPassword,
+      email,
+      isVendor: true,
+    });
+    await user.save();
+    res.status(200).json({ message: "vendor created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const vendorSignin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validVendor = await User.findOne({ email });
+    if (!validVendor || !validVendor.isVendor) return next(errorHandler(404, "user not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validVendor.password);
+    if (!validPassword) {
+      return next(errorHandler(404,"wrong credentials"));
+    }
+
+    const accessToken = Jwt.sign({ id: validVendor._id }, process.env.ACCESS_TOKEN, {
+      expiresIn: "15m",
+    });
+    const refreshToken = Jwt.sign({ id: validVendor._id }, process.env.REFRESH_TOKEN, {
+      expiresIn: "7d",
+    });
+
+    const updatedVendor = await User.findByIdAndUpdate(
+      validVendor._id,
+      { refreshToken },
+      { new: true }
+    );
+
+    const { password: hadshedPassword, ...rest } = updatedVendor._doc;
+    res.status(200).json({ ...rest, accessToken, refreshToken });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const vendorSignout = async (req, res, next) => {
+  try {
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json({ message: "vendor signedout successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+//vendor login or signup with google
+
+export const vendorGoogle = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user && !user.isVendor) return next(errorHandler(409, "email already in use as a user"));
+
+    if (user && user.isVendor) {
+      const accessToken = Jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN, { expiresIn: "15m" });
+      const refreshToken = Jwt.sign({ id: user._id }, process.env.REFRESH_TOKEN, { expiresIn: "7d" });
+      const updatedVendor = await User.findByIdAndUpdate(
+        user._id,
+        { refreshToken },
+        { new: true }
+      );
+
+      const { password: hashedPassword, ...rest } = updatedVendor._doc;
+      return res.status(200).json({ ...rest, accessToken, refreshToken });
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8); //we are generating a random password since there is no password in result
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        profilePicture: req.body.photo,
+        password: hashedPassword,
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-8) +
+          Math.random().toString(36).slice(-8),
+        email: req.body.email,
+        isVendor:true,
+        //we cannot set username to req.body.name because other user may also have same name so we generate a random value and concat it to name
+        //36 in toString(36) means random value from 0-9 and a-z
+      });
+      try{
+        const savedUser = await newUser.save();
+
+        const accessToken = Jwt.sign({ id: savedUser._id }, process.env.ACCESS_TOKEN, {
+          expiresIn: "15m",
+        });
+        const refreshToken = Jwt.sign({ id: savedUser._id }, process.env.REFRESH_TOKEN, {
+          expiresIn: "7d",
+        });
+
+        const updatedVendor = await User.findByIdAndUpdate(
+          savedUser._id,
+          { refreshToken },
+          { new: true }
+        );
+
+        const { password: hashedPassword2, ...rest } = updatedVendor._doc;
+        res.status(200).json({ ...rest, accessToken, refreshToken });
+      }
+      catch(error){
+        if(error.code === 11000){
+          return next(errorHandler(409,"email already in use"))
+        }
+        next(error)
+      }
+     
+    }
+  } catch (error) {
+    next(error);
+  }
+};
